e9ad64016768b4c8ca0cd10b381fc5ff2ce98ab3
ewciks <ewa.sobieniak@gmail.com>
2014-07-12 13:57:43 +0200

    Starting positions selection by page up/down + empty manual input option (at this moment without interaction)

--- collision/collision/collision/Game1.cs
+++ collision/collision/collision/Game1.cs
@@ -19,7 +19,7 @@ namespace collision
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        KeyboardState oldState;
+        KeyboardState oldKeyboardState;
 
         Collision collision;
         bool isNextCollisionTimeKnown;
@@ -30,7 +30,7 @@ namespace collision
         const float METER_TO_PIXEL_FACTOR = 2000.0f; // 1m = 2000 px, to have value in [m] you have to multiply it by METER_TO_PIXEL_FACTOR
         const float R_SCALE = 40.0f;    // for 100x100 px ball picture to have 1x1m picutre you have to multiply it by R_SCALE
         int ballsStartingPositionOption = 0;
-        const int BALLS_STARTING_POSITIONS = 5;
+        const int BALLS_STARTING_POSITIONS = 6;
 
         // printing bottom panel with data
         Texture2D options;
@@ -43,6 +43,7 @@ namespace collision
         string nextCollision;
         int selectedBallId;
         int selectedBall;
+        string optionsBallPositionsString;
 
         // pause printing
         bool isRunning;
@@ -50,7 +51,7 @@ namespace collision
         Vector2 pausePos;
         Vector2 pauseSize;
         Vector2 pauseStringPos;
-        const string pauseString = "PAUSE (press: )\n space - resume\n tab - switch balls\n key down - switch ball starting positions";
+        const string pauseString = "PAUSE (press: )\n space - resume\n tab - switch balls\n page up/down - switch ball starting positions";
 
         public Game1()
         {
@@ -58,6 +59,7 @@ namespace collision
             Content.RootDirectory = "Content";
             this.Window.Title = "Perfect elastic collisions 2D";
             this.IsMouseVisible = true;
+            Mouse.WindowHandle = Window.Handle;
         }
 
         private void UpdateKeyboardInput()
@@ -67,7 +69,7 @@ namespace collision
             if (newState.IsKeyDown(Keys.Space))
             {
                 // If not down last update, key has just been pressed.
-                if (!oldState.IsKeyDown(Keys.Space))
+                if (!oldKeyboardState.IsKeyDown(Keys.Space))
                 {
                     if (isRunning) isRunning = false;
                     else isRunning = true;
@@ -75,22 +77,37 @@ namespace collision
             }
             else if (newState.IsKeyDown(Keys.Tab))
             {
-                if (!oldState.IsKeyDown(Keys.Tab))
+                if (!oldKeyboardState.IsKeyDown(Keys.Tab))
                 {
                     selectedBall = (selectedBall + 1) % collision.Balls.Count;
                     selectedBallId = collision.Balls[selectedBall].Id;
                 }
             }
-            else if (newState.IsKeyDown(Keys.Down))
+            else if (newState.IsKeyDown(Keys.PageDown))
             {
-                if (!oldState.IsKeyDown(Keys.Down))
+                if (!oldKeyboardState.IsKeyDown(Keys.PageDown))
                 {
                     ballsStartingPositionOption = (ballsStartingPositionOption + 1) % BALLS_STARTING_POSITIONS;
                     StartNewSimulation();
                 }
             }
+            else if (newState.IsKeyDown(Keys.PageUp))
+            {
+                if (!oldKeyboardState.IsKeyDown(Keys.PageUp))
+                {
+                    ballsStartingPositionOption = (ballsStartingPositionOption - 1) % BALLS_STARTING_POSITIONS;
+                    StartNewSimulation();
+                }
+            }
+            /*else if (newState.IsKeyDown(Keys.Enter) && ballsStartingPositionOption == 5)    // manual input
+            {
+                if (!oldKeyboardState.IsKeyDown(Keys.Enter))
+                {
+                    collision.Balls.Add(new Ball2(Vector2.Zero, new Vector2(mouseState.X/1000.0f, mouseState.Y/1000.0f), 0.01f, 0.01f));
+                }
+            }*/
             // Update saved state.
-            oldState = newState;
+            oldKeyboardState = newState;
         }
 
         private void CalcAndSetNextCol()
@@ -120,20 +137,23 @@ namespace collision
         {
             balls.Add(new Ball2(new Vector2(0.05f, 0), new Vector2(0.1f, 0.01f), 0.01f, 0.01f));
             balls.Add(new Ball2(new Vector2(0.0f, 0), new Vector2(0.2f, 0.01f), 0.01f, 0.01f));
-            balls.Add(new Ball2(new Vector2(0.1f, 0), new Vector2(0.1f, 0.1f), 0.01f, 0.01f));
+            balls.Add(new Ball2(new Vector2(0.1f, 0), new Vector2(0.1f, 0.1f), 0.02f, 0.02f));
             balls.Add(new Ball2(new Vector2(-0.1f, 0), new Vector2(0.35f, 0.1f), 0.01f, 0.01f));
+            optionsBallPositionsString = "Horizontal collision.";
         }
 
         private void BallBallVerticalTest(List<Ball2> balls)
         {
             balls.Add(new Ball2(new Vector2(0.0f, 0.05f), new Vector2(0.1f, 0.01f), 0.01f, 0.01f));
-            balls.Add(new Ball2(new Vector2(0.0f, -0.1f), new Vector2(0.1f, 0.15f), 0.01f, 0.01f)); 
+            balls.Add(new Ball2(new Vector2(0.0f, -0.1f), new Vector2(0.1f, 0.15f), 0.01f, 0.01f));
+            optionsBallPositionsString = "Vertical collision.";
         }
 
         private void BallBallInclinedTest(List<Ball2> balls)
         {
             balls.Add(new Ball2(new Vector2(0.05f, 0.05f), new Vector2(0.1f, 0.01f), 0.01f, 0.01f));
             balls.Add(new Ball2(new Vector2(-0.1f, -0.1f), new Vector2(0.19f, 0.1f), 0.01f, 0.01f));
+            optionsBallPositionsString = "Inclined collision.";
         }
 
         private void BallBallTripleColTest(List<Ball2> balls)
@@ -141,6 +161,7 @@ namespace collision
             balls.Add(new Ball2(new Vector2(0.1f, 0.0f), new Vector2(0.01f, 0.11f), 0.01f, 0.01f));
             balls.Add(new Ball2(new Vector2(-0.1f, 0.0f), new Vector2(0.21f, 0.11f), 0.01f, 0.01f));
             balls.Add(new Ball2(new Vector2(0.0f, 0.1f), new Vector2(0.11f, 0.01f), 0.01f, 0.01f));
+            optionsBallPositionsString = "Triple collision.";
         }
 
         private void BillardStartTest(List<Ball2> balls)
@@ -154,7 +175,13 @@ namespace collision
 
             balls.Add(new Ball2(new Vector2(0.0f, 0.0f), new Vector2(0.06f + 0.02f, 0.10f), 0.01f, 0.01f));
 
-            balls.Add(new Ball2(new Vector2(-0.05f, 0.0f), new Vector2(0.3f, 0.10f), 0.01f, 0.01f));
+            balls.Add(new Ball2(new Vector2(-0.4f, 0.0f), new Vector2(0.3f, 0.10f), 0.01f, 0.01f));
+            optionsBallPositionsString = "Likewise billard start collision.";
+        }
+
+        private void BallsManualInput()
+        {
+            optionsBallPositionsString = "Manual input.";
         }
 
         private List<Ball2> AddBalls(int option)
@@ -177,8 +204,11 @@ namespace collision
                 case 4:
                     BillardStartTest(balls);
                     break;
+                case 5:
+                    BallsManualInput();
+                    break;
                 default:
-                    BillardStartTest(balls);
+                    BallsManualInput();
                     break;
             }
             return balls;
@@ -210,7 +240,8 @@ namespace collision
             List<Wall2> walls = AddWallsAsRectangle();
 
             collision = new Collision(balls, walls);
-            selectedBallId = collision.Balls[0].Id;
+            if (collision.Balls.Count > 0) selectedBallId = collision.Balls[0].Id;
+            else selectedBallId = 0;
             kin_energy = collision.CalcTotalKineticEnergy();
             CalcAndSetNextCol();
         }
@@ -280,9 +311,6 @@ namespace collision
                 collision.MoveBallsToTime(elapsed);
                 if (isNextCollisionTimeKnown && collisionTimer <= 0)
                 {
-                    //if (counter == 127)
-                    //{
-                    //}
                     collision.MoveBallsToTime(collisionTimer);
                     foreach (NextCollision nc in collision.NextCollisions)
                     {
@@ -320,7 +348,7 @@ namespace collision
             foreach (Ball2 b in collision.Balls)
             {
                 Vector2 position = b.Coordinates * METER_TO_PIXEL_FACTOR;
-                Vector2 origin = new Vector2(2.0f * b.R * METER_TO_PIXEL_FACTOR, 2.0f * b.R * METER_TO_PIXEL_FACTOR);
+                Vector2 origin = new Vector2(b.R * METER_TO_PIXEL_FACTOR, b.R * METER_TO_PIXEL_FACTOR);
                 float scale = R_SCALE * b.R;
                 spriteBatch.Draw(ballsSpriteTextureBlack, position, null, Color.White, 0.0f, origin, scale, SpriteEffects.None, 0);
                 if (b.Id == selectedBallId)
@@ -336,10 +364,10 @@ namespace collision
                     Ball2 b1 = (Ball2)nc.Obj1;
                     Ball2 b2 = (Ball2)nc.Obj2;
                     Vector2 position1 = b1.Coordinates * METER_TO_PIXEL_FACTOR;
-                    Vector2 origin1 = new Vector2(2.0f * b1.R * METER_TO_PIXEL_FACTOR, 2.0f * b1.R * METER_TO_PIXEL_FACTOR);
+                    Vector2 origin1 = new Vector2(b1.R * METER_TO_PIXEL_FACTOR, b1.R * METER_TO_PIXEL_FACTOR);
                     float scale1 = R_SCALE * b1.R;
                     Vector2 position2 = b2.Coordinates * METER_TO_PIXEL_FACTOR;
-                    Vector2 origin2 = new Vector2(2.0f * b2.R * METER_TO_PIXEL_FACTOR, 2.0f * b2.R * METER_TO_PIXEL_FACTOR);
+                    Vector2 origin2 = new Vector2(b2.R * METER_TO_PIXEL_FACTOR, b2.R * METER_TO_PIXEL_FACTOR);
                     float scale2 = R_SCALE * b2.R;
                     spriteBatch.Draw(ballsSpriteTextureRed, position1, null, Color.White, 0.0f, origin1, scale1, SpriteEffects.None, 0);
                     spriteBatch.Draw(ballsSpriteTextureRed, position2, null, Color.White, 0.0f, origin2, scale2, SpriteEffects.None, 0);
@@ -351,16 +379,17 @@ namespace collision
                 if (b.Id == selectedBallId)
                 {
                     Vector2 position = b.Coordinates * METER_TO_PIXEL_FACTOR;
-                    Vector2 origin = new Vector2(2.0f * b.R * METER_TO_PIXEL_FACTOR, 2.0f * b.R * METER_TO_PIXEL_FACTOR);
+                    Vector2 origin = new Vector2(b.R * METER_TO_PIXEL_FACTOR, b.R * METER_TO_PIXEL_FACTOR);
                     float scale = R_SCALE * b.R;
                     spriteBatch.Draw(ballsSpriteTextureYellow, position, null, Color.White, 0.0f, origin, scale, SpriteEffects.None, 0);
                 }
             }
             spriteBatch.Draw(options, new Rectangle((int)optionsPos.X, (int)optionsPos.Y, (int)optionsSize.X, (int)optionsSize.Y), Color.WhiteSmoke);
-            spriteBatch.DrawString(font, "Collisions number: " + counter.ToString(), fontPos, Color.Green);
-            spriteBatch.DrawString(font, "Total kinetic energy: " + kin_energy.ToString() + "[J]", fontPos + new Vector2(0, font.LineSpacing), Color.Green);
-            spriteBatch.DrawString(font, "Next collision:" + nextCollision, fontPos + new Vector2(0, 2 * font.LineSpacing), Color.Green);
-            if(collision.Balls.Count > 0) spriteBatch.DrawString(font, "Selected ball: " + collision.Balls.Find(b => b.Id == selectedBallId).ToString(), fontPos + new Vector2(0, 3 * font.LineSpacing), Color.Green);
+            spriteBatch.DrawString(font, "Starting position: " + optionsBallPositionsString, fontPos, Color.Green);
+            spriteBatch.DrawString(font, "Collisions number: " + counter.ToString(), fontPos + new Vector2(0, font.LineSpacing), Color.Green);
+            spriteBatch.DrawString(font, "Total kinetic energy: " + kin_energy.ToString() + "[J]", fontPos + new Vector2(0, 2 * font.LineSpacing), Color.Green);
+            spriteBatch.DrawString(font, "Next collision:" + nextCollision, fontPos + new Vector2(0, 3 * font.LineSpacing), Color.Green);
+            if(collision.Balls.Count > 0) spriteBatch.DrawString(font, "Selected ball: " + collision.Balls.Find(b => b.Id == selectedBallId).ToString(), fontPos + new Vector2(0, 4 * font.LineSpacing), Color.Green);
             if (!isRunning)
             {
                 spriteBatch.Draw(pause, new Rectangle((int)pausePos.X, (int)pausePos.Y, (int)pauseSize.X, (int)pauseSize.Y), Color.Gray * 0.5f);
